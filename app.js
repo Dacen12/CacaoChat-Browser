70fe85132b23c3dd1a4cde45ced58fb21d6ce6e3
dacen12 <alkisali1@hotmail.com>
2021-04-24 21:25:41 +0100

    rebranded

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { json } = require('express')
+
 const express = require('express')
 const http = require('http')
 const path = require('path')
@@ -7,10 +7,10 @@ const server  = http.createServer(app)
 const socket = require('socket.io')(server)
 
 
-const {joinUserToRoom} = require('./client/class/User')
+const {joinUserToRoom} = require('./client/User')
 
 app.use(express.json())
-console.log("updated version!")
+
 socket.on("connection", socket => {
 
         socket.on('MobileRoomJoined', (username, mobile_room) => {
@@ -25,19 +25,13 @@ socket.on("connection", socket => {
     })
 
     socket.on('roomJoined', ({username, room}) => {
-        console.log(`computer : ${username} +  ${room}`)
-         socket.join(room)
-        
-       const user = joinUserToRoom(socket.id, username ,room)
-      console.log(`user is : ${user.username} inside room : ${user.room}`)
-        
+        socket.join(room)
+       
+        const user = joinUserToRoom(socket.id, username ,room) 
        socket.on('messageToOthers', ({username, message})=> {
-           console.log("inside message from others: " +username + 'and the message is: ' + message)
            socket.broadcast.in(user.room).emit('receivedMessage', ({username, message}))
        })
-    
     })
-
 })
 
 app.use(express.static(path.join(__dirname, '/client/'), {index: 'index.html'}))
